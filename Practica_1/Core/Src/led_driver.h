95eaea745b62530125126278f7bc0025a4d94485
jorcallag <jorcallag>
2021-09-30 18:25:58 +0200

    Comienzo Practica 1

---
+++ Practica_1/Core/Src/led_driver.h
@@ -0,0 +1,15 @@
+/*
+ * led_driver.h
+ *
+ *  Created on: Sep 27, 2021
+ *      Author: Jorlu
+ */
+
+#ifndef SRC_LED_DRIVER_H_
+#define SRC_LED_DRIVER_H_
+
+void LED_ON(uint8_t led);
+void LED_OFF(uint8_t led);
+void LED_TOGGLE(uint8_t led);
+
+#endif /* SRC_LED_DRIVER_H_ */
