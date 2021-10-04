4fbcb1e10726abc705e54f856daad21a428af4b1
jorcallag <jorcallag>
2021-10-04 13:54:10 +0200

    P01 v3

---
+++ Practica_1/Core/Src/button_driver.h
@@ -0,0 +1,13 @@
+/*
+ * button_driver.h
+ *
+ *  Created on: Oct 4, 2021
+ *      Author: Jorlu
+ */
+
+#ifndef SRC_BUTTON_DRIVER_H_
+#define SRC_BUTTON_DRIVER_H_
+
+uint8_t ReadButton(void);
+
+#endif /* SRC_BUTTON_DRIVER_H_ */
